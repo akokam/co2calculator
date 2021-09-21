fe7712c414c349d2d14af77242ab44272a1813c4
Christina Ludwig <christina_ludwig@gmx.net>
2021-09-21 15:52:15 +0200

    added enum for commuting transportation modes

--- co2calculator/constants.py
+++ co2calculator/constants.py
@@ -7,6 +7,16 @@ import enum
 KWH_TO_TJ = 277777.77777778
 
 
+class CommutingTransportationMode(enum.Enum):
+    CAR = 'Car'
+    BUS = 'Bus'
+    TRAIN = 'Train'
+    BIKE = 'Bike'
+    EBIKE = 'E-bike'
+    MOTORBIKE = 'Motorbike'
+    TRAM = 'Tram'
+
+
 class HeatingFuel(enum.Enum):
     """Enum for heating fuel types"""
 
